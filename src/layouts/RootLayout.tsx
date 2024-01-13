e83b7fe3415b5447a5e30cc17d2461ced3e4b8d2
Stahann <stahankhmil@gmail.com>
2024-01-13 16:02:28 +0200

    new styles

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -4,7 +4,6 @@ import { Outlet } from 'react-router-dom'
 import { Box } from '@mui/material'
 import Navbar from '../components/nav/Navbar'
 import Drawer from '../components/nav/Drawer'
-// import { PersistentDrawer as Drawer } from '../components/nav/Drawer'
 import { Main } from '../components/nav/Main'
 
 const RootLayout = () => {
@@ -14,9 +13,16 @@ const RootLayout = () => {
 
   return (
     <>
-      <Box>
+      <Box sx={{ display: 'flex', height: '100%' }}>
         <Drawer open={open} toggle={toggleDrawer} />
-        <Main sx={{ bgcolor: 'tomato' }}>
+        <Main
+          sx={{
+            bgcolor: 'tomato',
+            flexGrow: '1',
+            padding: '24px',
+            marginLeft: open ? '0px' : '-240px',
+          }}
+        >
           <Outlet />
         </Main>
       </Box>
