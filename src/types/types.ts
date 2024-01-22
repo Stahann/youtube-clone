a74103e9f00dca94b94bdb2e112496b564792dbe
Stahann <stahankhmil@gmail.com>
2024-01-22 19:39:47 +0200

    Add new type

--- src/types/types.ts
+++ src/types/types.ts
@@ -70,3 +70,92 @@ export interface IChannel {
   id: string
   kind: string
 }
+
+export interface IVideoDetail {
+  contentDetails: {
+    caption: string
+    definition: string
+    dimension: string
+    duration: string
+    licensedContent: boolean
+    projection: string
+  }
+  id: string
+  kind: string
+  snippet: {
+    categoryId: string
+    channelId: string
+    channelTitle: string
+    defaultAudioLanguage: string
+    defaultLanguage: string
+    description: string
+    liveBroadcastContent: string
+    localized: {
+      description: string
+      title: string
+    }
+    publishedAt: string
+    tags: {
+      0: string
+      1: string
+      2: string
+      3: string
+      4: string
+      5: string
+      6: string
+      7: string
+      8: string
+      9: string
+      10: string
+      11: string
+      12: string
+      13: string
+      14: string
+      15: string
+      16: string
+      17: string
+      18: string
+      19: string
+      20: string
+      21: string
+      22: string
+      23: string
+      24: string
+      25: string
+    }
+    thumbnails: {
+      default: {
+        height: number
+        url: string
+        width: number
+      }
+      high: {
+        height: number
+        url: string
+        width: number
+      }
+      maxres: {
+        height: number
+        url: string
+        width: number
+      }
+      medium: {
+        height: number
+        url: string
+        width: number
+      }
+      standard: {
+        height: number
+        url: string
+        width: number
+      }
+    }
+    title: string
+  }
+  statistics: {
+    commentCount: string
+    favoriteCount: string
+    likeCount: string
+    viewCount: string
+  }
+}
